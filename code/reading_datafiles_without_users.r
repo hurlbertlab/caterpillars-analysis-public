92708b24be488ce09193020309d375f522dfee14
Allen Hurlbert <hurlbert@bio.unc.edu>
2018-10-08 12:05:43 -0400

    separating scripts for reading in data, calcing summary stats

---
+++ code/reading_datafiles_without_users.r
@@ -0,0 +1,35 @@
+# Reading in Caterpillars Count! database files
+library(dplyr)
+library(lubridate)
+
+# Function for substituting values based on a condition using dplyr::mutate
+# Modification of dplyr's mutate function that only acts on the rows meeting a condition
+mutate_cond <- function(.data, condition, ..., envir = parent.frame()) {
+  condition <- eval(substitute(condition), .data, envir)
+  .data[condition, ] <- .data[condition, ] %>% mutate(...)
+  .data
+}
+
+
+# Read in data files
+sites = read.csv(paste('data/', list.files('data')[grep('Site.csv', list.files('data'))], sep = ''), header = TRUE, stringsAsFactors = FALSE)
+
+surveys = read.csv(paste('data/', list.files('data')[grep('Survey.csv', list.files('data'))], sep = ''), header = TRUE, stringsAsFactors = FALSE)
+
+arths = read.csv(paste('data/', list.files('data')[grep('ArthropodSighting.csv', list.files('data'))], sep = ''), header = TRUE, stringsAsFactors = FALSE)
+
+plants = read.csv(paste('data/', list.files('data')[grep('Plant.csv', list.files('data'))], sep = ''), header = TRUE, stringsAsFactors = FALSE)
+
+surveys$LocalDate = as.Date(surveys$LocalDate, format = "%Y-%m-%d")
+surveys$Year = format(surveys$LocalDate, "%Y")
+surveys$julianday = yday(surveys$LocalDate)
+
+
+fullDataset = surveys %>%
+  select(ID, UserFKOfObserver, PlantFK, LocalDate, julianday, Year, ObservationMethod, Notes, WetLeaves, PlantSpecies, NumberOfLeaves,
+         AverageLeafLength, HerbivoryScore) %>%
+  left_join(arths[, !names(arths) %in% "PhotoURL"], by = c('ID' = 'SurveyFK')) %>%
+  left_join(plants, by = c('PlantFK' = 'ID')) %>%
+  left_join(sites[, c('ID', 'Name', 'Latitude', 'Longitude', 'Region')], by = c('SiteFK' = 'ID')) %>% 
+  mutate_cond(is.na(Quantity), Quantity = 0, Group) %>%
+  rename(surveyNotes = Notes.x, bugNotes = Notes.y, arthID = ID.y)
---
+++ code/summaryStats.r
@@ -0,0 +1,45 @@
+# Project Summary Statistics
+
+summaryStats = function(reportYear = format(Sys.Date(), "%Y")) {
+  source(list.files('code')[grep('reading_datafiles', list.files('code'))]
+  
+  stats = list(
+    numSurveysTotal = nrow(fullDataset),
+    
+    numSurveysThisYear = fullDataset %>% filter(Year == reportYear) %>% nrow(),
+    
+    sumSitesTotal = fullDataset %>% summarize(n = n_distinct(SiteFK)),
+    
+    sumSitesThisYear = fullDataset %>% filter(Year == reportYear) %>% summarize(n = n_distinct(SiteFK)),
+    
+    numUsers = fullDataset %>% summarize(n = n_distinct(UserFKOfObserver)), 
+    
+    numUsersThisYear = fullDataset %>% filter(Year == reportYear) %>% 
+      summarize(n = n_distinct(UserFKOfObserver)),
+    
+    arthTot = fullDataset %>% summarize(n = sum(Quantity, na.rm = TRUE)),
+    
+    arthTotThisYear = fullDataset %>% filter(Year == reportYear) %>% 
+      summarize(n = sum(Quantity, na.rm = TRUE)),
+    
+    caterpillarTot = fullDataset %>% filter(Group == "caterpillar") %>% 
+      summarize(n = sum(Quantity, na.rm = TRUE)),
+    
+    caterpillarTotThisYear = fullDataset %>% 
+      filter(Year == reportYear, Group == "caterpillar") %>% 
+      summarize(n = sum(Quantity, na.rm = TRUE)),
+    
+    medianNumSurveysPerSite = plants %>%
+      count(SiteFK) %>%
+      summarize(median = median(n)) %>%
+      data.frame(),
+    
+    medianNumDatesPerSiteThisYear = fullDataset %>%
+      filter(Year == reportYear) %>%
+      distinct(Name, LocalDate) %>%
+      count(Name) %>%
+      summarize(median = median(n)) %>%
+      data.frame()
+  )
+  return(stats)
+}
