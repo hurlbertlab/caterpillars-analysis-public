47e0fbd354853c4598ecdeaea98ae9d9ba7c3c57
Di Cecco <gdicecco@ad.unc.edu>
2018-10-12 15:35:05 -0400

    make dots larger

--- code/phenology.r
+++ code/phenology.r
@@ -263,7 +263,7 @@ ggplot() + geom_polygon(data = eastern_states, aes(x = long, y = lat, group = gr
   labs(x = NULL, y = NULL, size = "Number of surveys", color = "Mean peak in caterpillars") +
   ylim(31, 50) +
   scale_color_viridis(option = "C", breaks = summer$jds, labels = summer$month, limits = c(150, 215)) +
-  scale_size(range=c(2, 5))
+  scale_size(range=c(2.5, 5))
 dev.off()
 
 # Plot of peak date in June/July window
@@ -291,5 +291,5 @@ ggplot() + geom_polygon(data = eastern_states, aes(x = long, y = lat, group = gr
   labs(x = NULL, y = NULL, size = "Number of surveys", color = "Peak in caterpillars") +
   ylim(31, 50) +
   scale_color_viridis(option = "C", breaks = summer$jds, labels = summer$month, limits = c(150, 215)) +
-  scale_size(range=c(2, 5))
+  scale_size(range=c(2.5, 5))
 dev.off()
