0832cf8bd17b88f18ae4f26e76520f5777896f56
Di Cecco <gdicecco@ad.unc.edu>
2018-10-12 15:21:12 -0400

    Revise figures, add figures for absolute peak

--- code/phenology.r
+++ code/phenology.r
@@ -181,7 +181,7 @@ library(ggplot2)
 library(viridis)
 
 # Create nested data frame containing top 12 sites, get mean density by day of caterpillars for each
-# Calculate mean peak in caterpillar abundance based on fraction of surveys
+# Calculate mean peak and absolute peak in % surveys containing caterpillars
 t12_nested <- fullDataset %>%
   filter(Name %in% siteList$Name) %>%
   group_by(Name) %>%
@@ -192,6 +192,28 @@ t12_nested <- fullDataset %>%
     df <- .
     sum(df$fracSurveys*df$julianday)/sum(df$fracSurveys) # weighted mean of survey day by fraction of surveys with caterpillars
   })) %>%
+  mutate(minJD = map_dbl(meanDens, ~{
+    df <- .
+    min(df$julianday) # First survey date
+  })) %>%
+  mutate(maxJD = map_dbl(meanDens, ~{
+    df <- .
+    max(df$julianday) # Last survey date
+  })) %>%
+  mutate(weightedPeakJunJul = map_dbl(meanDens, ~{
+    df <- .
+    commonTime <- filter(df, julianday >= 157 & julianday <= 205) # JD time period all surveys overlap - June and July
+    sum(commonTime$fracSurveys*commonTime$julianday)/sum(commonTime$fracSurveys) # weighted mean of survey day by fraction of surveys with caterpillars
+  })) %>%
+  mutate(peak = map_dbl(meanDens, ~{
+    df <- .
+    df[which.max(df$fracSurveys), "julianday"] # Day of max peak in % surveys with caterpillars
+  })) %>%
+  mutate(peakJunJul = map_dbl(meanDens, ~{
+    df <- .
+    commonTime <- filter(df, julianday >= 157 & julianday <= 205)
+    commonTime[which.max(commonTime$fracSurveys), "julianday"] # Day of max peak in % surveys with caterpillars in June/July
+  }))   %>%
   left_join(siteList) %>%
   left_join(unique(fullDataset[, c("Name", "Longitude")]))
 
@@ -205,24 +227,30 @@ dates = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", 
 year <- data.frame(jds = jds, month = dates)
 summer <- filter(year, jds > 100 & jds < 220)
 
-# Plot of weighted mean peak in fraction of surveys with caterpillars by latitude
-pdf('figs/weightedCaterpillarPeakScatter2018.pdf', height = 8.5, width = 11)
-ggplot(t12_nested, aes(x = Latitude, y = weightedPeak)) + geom_point(aes(size = nRecs), col = "dodgerblue3") +
-  labs(y = "Mean peak in caterpillars", size = "N. Surveys") + xlim(32, 49) + 
-  scale_y_continuous(breaks = summer$jds, labels = summer$month)
+# Maps of US and Canada
+library(raster)
+canada <- getData("GADM", country="CAN", level = 1)
+usa <- getData("GADM", country = "USA", level = 1) # figure out how to remove water
+
+ontario <- subset(canada, NAME_1 == "Ontario")
+eastern_us <- subset(usa, NAME_1 %in% c("Michigan", "Indiana", "Kentucky", "Tennessee", "North Carolina", "Virginia",
+                                        "West Virginia", "Ohio", "Maryland", "Delaware","New Jersey", "Pennsylvania",
+                                        "New York", "Rhode Island", "Connecticut", "Massachusetts", "New Hampshire",
+                                        "Vermont", "Maine", "South Carolina"))
+
+# Plot of weighted mean peak in fraction of surveys with caterpillars by latitude, using only June and July
+pdf('figs/weightedCaterpillarPeakScatter2018.pdf', height = 6, width = 8)
+ggplot(t12_nested, aes(x = Latitude, y = weightedPeakJunJul)) + geom_point(aes(size = nRecs), col = "dodgerblue3") +
+  labs(y = "Mean peak in caterpillars", size = "N. Surveys") + xlim(32, 49) +
+  scale_y_continuous(breaks = summer$jds, labels = summer$month, limits = c(150, 215)) +
+  scale_size(range = c(2, 7))
 # add site names: geom_text(aes(x = Latitude + 1, y = weightedPeak - 2, label = Name))
 dev.off()
 
 # Map of same info
-us_states <- map_data("state")
-eastern_states <- us_states %>%
-  filter(region %in% c("michigan", "indiana", "kentucky", "tennessee", "north carolina", "virginia",
-                       "west virginia", "ohio", "maryland", "delaware","new jersey", "pennsylvania",
-                       "new york", "rhode island", "connecticut", "massachusetts", "new hampshire",
-                       "vermont", "maine", "south carolina"))
-
 pdf('figs/weightedCaterpillarPeakMap2018.pdf', height = 8.5, width = 11)
 ggplot() + geom_polygon(data = eastern_states, aes(x = long, y = lat, group = group), fill = "gray88", color = "white") + 
+  geom_polygon(data = ontario, aes(x = long, y = lat, group = group), fill = "gray90", color = "white") +
   coord_map() +
   theme(panel.border = element_blank()) + 
   theme(panel.background = element_blank()) +
@@ -230,7 +258,35 @@ ggplot() + geom_polygon(data = eastern_states, aes(x = long, y = lat, group = gr
   theme(axis.ticks = element_blank()) +
   theme(axis.text = element_blank()) +
   geom_point(data = t12_nested, 
-             aes(x = Longitude, y = Latitude, size = nRecs, color = weightedPeak)) +
+             aes(x = Longitude, y = Latitude, size = nRecs, color = weightedPeakJunJul)) +
   labs(x = NULL, y = NULL, size = "Number of surveys", color = "Mean peak in caterpillars") +
-  scale_color_viridis(option = "C", breaks = summer$jds, labels = summer$month)
+  scale_color_viridis(option = "C", breaks = summer$jds, labels = summer$month, limits = c(150, 215)) +
+  scale_size(range=c(2.5, 5))
+dev.off()
+
+# Plot of peak date in June/July window
+pdf('figs/CaterpillarPeakScatter2018.pdf', height = 6, width = 8)
+ggplot(t12_nested, aes(x = Latitude, y = peakJunJul)) + geom_point(aes(size = nRecs), col = "dodgerblue3") +
+  labs(y = "Peak in caterpillars", size = "N. Surveys") + xlim(32, 49) +
+  scale_y_continuous(breaks = summer$jds, labels = summer$month, limits = c(150, 215)) +
+  scale_size(range = c(2, 7)) +
+  theme(panel.grid = element_blank())
+# add site names: geom_text(aes(x = Latitude + 1, y = weightedPeak - 2, label = Name))
+dev.off()
+
+# Map of same info
+pdf('figs/CaterpillarPeakMap2018.pdf', height = 8.5, width = 11)
+ggplot() + geom_polygon(data = eastern_states, aes(x = long, y = lat, group = group), fill = "gray88", color = "white") + 
+  geom_polygon(data = ontario, aes(x = long, y = lat, group = group), fill = "gray90", color = "white") +
+  coord_map() +
+  theme(panel.border = element_blank()) + 
+  theme(panel.background = element_blank()) +
+  theme(panel.grid = element_blank()) + 
+  theme(axis.ticks = element_blank()) +
+  theme(axis.text = element_blank()) +
+  geom_point(data = t12_nested, 
+             aes(x = Longitude, y = Latitude, size = nRecs, color = peakJunJul)) +
+  labs(x = NULL, y = NULL, size = "Number of surveys", color = "Peak in caterpillars") +
+  scale_color_viridis(option = "C", breaks = summer$jds, labels = summer$month, limits = c(150, 215)) +
+  scale_size(range=c(2.5, 5))
 dev.off()
