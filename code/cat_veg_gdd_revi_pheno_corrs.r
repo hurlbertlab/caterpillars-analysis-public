4ce0573e33782b802716480d44ddaccd45963a4b
Allen Hurlbert <hurlbert@bio.unc.edu>
2018-12-06 16:12:28 -0500

    script for examining correlations among pheno dates btw cats, veg, gdd, revi

---
+++ code/cat_veg_gdd_revi_pheno_corrs.r
@@ -0,0 +1,49 @@
+# This file creates a vertically stacked set of caterpillar phenology curves
+# based on Caterpillars Count! data from 2018 for an AGU poster
+
+library(dplyr)
+library(lubridate)
+library(purrr)
+
+
+source('code/analysis_functions.r')
+source('code/CCrawdata2masterdataframe.r')
+
+# Read in sites
+sitedata = read.table('data/sites_gdd_veg_bird_phenology.txt', header = T, sep = '\t', quote = '\"') 
+
+# Caterpillar phenology metrics
+summaryCC <- fullDataset %>%
+  filter(Name %in% sitedata$Name) %>%
+  group_by(Name) %>%
+  nest() %>%
+  mutate(meanDens = purrr::map(data, ~{meanDensityByDay(., ordersToInclude = 'caterpillar', 
+                                                        plot = FALSE, plotVar = 'fracSurveys')})) %>%
+  mutate(weightedPeak = map_dbl(meanDens, ~{
+    df <- .
+    sum(df$fracSurveys*df$julianday)/sum(df$fracSurveys) # weighted mean of survey day by fraction of surveys with caterpillars
+  })) %>%
+  mutate(minJD = map_dbl(meanDens, ~{
+    df <- .
+    min(df$julianday) # First survey date
+  })) %>%
+  mutate(maxJD = map_dbl(meanDens, ~{
+    df <- .
+    max(df$julianday) # Last survey date
+  })) %>%
+  mutate(weightedPeakJunJul = map_dbl(meanDens, ~{
+    df <- .
+    commonTime <- filter(df, julianday >= 157 & julianday <= 205) # JD time period all surveys overlap - June and July
+    sum(commonTime$fracSurveys*commonTime$julianday)/sum(commonTime$fracSurveys) # weighted mean of survey day by fraction of surveys with caterpillars
+  })) %>%
+  mutate(peak = map_dbl(meanDens, ~{
+    df <- .
+    df[which.max(df$fracSurveys), "julianday"] # Day of max peak in % surveys with caterpillars
+  })) %>%
+  mutate(peakJunJul = map_dbl(meanDens, ~{
+    df <- .
+    commonTime <- filter(df, julianday >= 157 & julianday <= 205)
+    commonTime[which.max(commonTime$fracSurveys), "julianday"] # Day of max peak in % surveys with caterpillars in June/July
+  }))   %>%
+  left_join(siteList) %>%
+  left_join(unique(fullDataset[, c("Name", "Longitude")]))
