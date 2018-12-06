a606898e3e7c3ad6c33ce47e66b282851183d46a
Allen Hurlbert <hurlbert@bio.unc.edu>
2018-12-06 09:34:21 -0500

    added latlong2county function

--- code/analysis_functions.r
+++ code/analysis_functions.r
@@ -60,3 +60,32 @@ meanDensityByDay = function(surveyData, # merged dataframe of Survey and arthrop
 }
 
 
+### Get county name from lat-longs
+library(sp)
+library(maps)
+library(maptools)
+
+# From https://stackoverflow.com/questions/13316185/r-convert-zipcode-or-lat-long-to-county
+# Note: had to remove proj4string references
+
+# The single argument to this function, pointsDF, is a data.frame in which:
+#   - column 1 contains the longitude in degrees (negative in the US)
+#   - column 2 contains the latitude in degrees
+
+latlong2county <- function(pointsDF) {
+  # Prepare SpatialPolygons object with one SpatialPolygon
+  # per county
+  counties <- map('county', fill=TRUE, col="transparent", plot=FALSE)
+  IDs <- sapply(strsplit(counties$names, ":"), function(x) x[1])
+  counties_sp <- map2SpatialPolygons(counties, IDs=IDs)
+  
+  # Convert pointsDF to a SpatialPoints object 
+  pointsSP <- SpatialPoints(pointsDF)
+  
+  # Use 'over' to get _indices_ of the Polygons object containing each point 
+  indices <- over(pointsSP, counties_sp)
+  
+  # Return the county names of the Polygons object containing each point
+  countyNames <- sapply(counties_sp@polygons, function(x) x@ID)
+  countyNames[indices]
+}
