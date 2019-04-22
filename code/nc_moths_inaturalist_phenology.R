5e23fc11079960da5f480f794bdcdb3e5fb34976
Di Cecco <gdicecco@ad.unc.edu>
2019-04-22 16:27:55 -0400

    inaturalist phenology curves for years 2015-2018

--- code/nc_moths_inaturalist_phenology.R
+++ code/nc_moths_inaturalist_phenology.R
@@ -75,7 +75,7 @@ for(yr in years){
     tm_shape(NC) + tm_borders() +
     tm_layout(title = as.character(yr), legend.text.size = 1, legend.title.size = 1.5)
   mnc_tm
-#  tmap_save(mnc_tm, paste0("figs/NC_moths_countyMap_", as.character(yr), ".pdf"))
+#  tmap_save(mnc_tm, paste0("figs/moths-nc/NC_moths_countyMap_", as.character(yr), ".pdf"))
 
 # NC iNaturalist obs
 
@@ -94,7 +94,7 @@ nc_inat <- inat_adults %>%
 inat_nc_tmap <- tm_shape(NC) + tm_borders() + tm_shape(nc_inat) + tm_dots() +
   tm_layout(title = as.character(yr))
 inat_nc_tmap
-#tmap_save(inat_nc_tmap, paste0("figs/NC_iNat_adultMoths_Map_", as.character(yr), ".pdf"))
+#tmap_save(inat_nc_tmap, paste0("figs/inaturalist/NC_iNat_adultMoths_Map_", as.character(yr), ".pdf"))
 
 # Phenology curves for these two, group by 1 degree lat lon bins
 # Threshold: 30 data points per spatial bin, march through june (jday 60-181)
@@ -155,7 +155,7 @@ ggplot(nc_moths_comb, aes(x = jd_wk, y = nMoths, col = data_source)) +
   theme(axis.text = element_text(size = 14), axis.title.y = element_text(size = 15),
         legend.text = element_text(size = 14), legend.title = element_text(size = 14),
         strip.text = element_text(size = 15), panel.spacing = unit(1, "lines"))
-ggsave(paste0("figs/phenocurves_iNat_NCMoths_", as.character(yr), ".pdf"), units = "in", width = 14, height = 5)
+ggsave(paste0("figs/cross-comparisons/phenocurves_iNat_NCMoths_", as.character(yr), ".pdf"), units = "in", width = 14, height = 5)
 }
 
 # Correlation of deltas
@@ -187,26 +187,26 @@ ggplot(nc_moth_deltas, aes(x = deltaNC, y = deltaiNat)) +
   geom_point() + geom_abline(slope = 1, intercept = 0, lty = 2) + 
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   facet_grid(lat_bin ~ lon_bin) + theme_bw()
-ggsave("figs/pheno_deltas_NCMoths_iNat.pdf")
+ggsave("figs/cross-comparisons/pheno_deltas_NCMoths_iNat.pdf")
 
 ###### Caterpillars <-> Adults: iNaturalist vs. iNaturalist ########
 
 begin <- 0
-end <- 243
+end <- 264
 minNumWks <- 10
 
 adult_moths <- inat_adults %>%
   mutate(Date = as.Date(observed_on, format = "%m/%d/%Y")) %>%
   mutate(year = year(Date), 
          jday = yday(Date)) %>%
-  filter(year == 2018) %>%
+  filter(year >= 2015) %>%
   mutate(lat_bin = 2*floor(latitude/2) + 2/2,
          lon_bin = 2*floor(longitude/2) + 2/2,
          jd_wk = 7*floor(jday/7)) %>%
-  group_by(lat_bin, lon_bin, jd_wk) %>%
+  group_by(lat_bin, lon_bin, year, jd_wk) %>%
   summarize(moths = n()) %>%
   filter(jd_wk >= begin, jd_wk <= end) %>%
-  group_by(lat_bin, lon_bin) %>%
+  group_by(lat_bin, lon_bin, year) %>%
   filter(n_distinct(jd_wk) >= minNumWks)
 
 caterpillars <- inat_cats %>%
@@ -215,23 +215,23 @@ caterpillars <- inat_cats %>%
   mutate(Date = as.Date(observed_on, format = "%Y-%m-%d")) %>%
   mutate(year = year(Date), 
          jday = yday(Date)) %>%
-  filter(year == 2018) %>%
+  filter(year >= 2015) %>%
   mutate(lat_bin = 2*floor(latitude/2) + 2/2,
          lon_bin = 2*floor(longitude/2) + 2/2,
          jd_wk = 7*floor(jday/7)) %>%
-  group_by(lat_bin, lon_bin, jd_wk) %>%
+  group_by(lat_bin, lon_bin, year, jd_wk) %>%
   summarize(caterpillars = n()) %>%
   filter(jd_wk >= begin, jd_wk <= end) %>%
-  group_by(lat_bin, lon_bin) %>%
+  group_by(lat_bin, lon_bin, year) %>%
   filter(n_distinct(jd_wk) >= minNumWks)
 
 inat_combined <- adult_moths %>%
-  full_join(caterpillars, by = c("lat_bin", "lon_bin", "jd_wk")) %>%
+  full_join(caterpillars, by = c("lat_bin", "lon_bin", "year", "jd_wk")) %>%
   filter(lat_bin > 24)
 
 inat_combined_gather <- inat_combined %>%
-  group_by(lat_bin, lon_bin) %>%
-  filter(sum(caterpillars, na.rm = T) > 0) %>%
+  group_by(lat_bin, lon_bin, year) %>%
+  filter(sum(caterpillars, na.rm = T) > 50) %>%
   gather(key = 'life_stage', value = "nObs", moths, caterpillars)
 
 # Phenology curves, group by 2 degree lat lon bins
@@ -241,49 +241,63 @@ bins <- inat_combined_gather %>%
 bins$group <- row.names(bins)
 
 inat_combined_ids <- inat_combined_gather %>%
-  left_join(bins, by = c("lat_bin", "lon_bin"))
+  left_join(bins, by = c("lat_bin", "lon_bin", "year"))
 
 # Raw correlations of phenology
-
-ggplot(inat_combined_gather, aes(x = jd_wk, y = nObs, col = life_stage)) +
-  geom_line(cex = 1) + 
-  scale_color_manual(values=c("deepskyblue3", "springgreen3"), 
-                     labels = c("caterpillars" = "Caterpillars", "moths" = "Moths")) +
-  scale_y_log10() +
-  facet_grid(forcats::fct_reorder(factor(lat_bin), desc(lat_bin))~lon_bin) +
-  scale_x_continuous(breaks = jds, labels = dates) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 12),
-        strip.text = element_text(size = 20), legend.text = element_text(size = 20),
-        legend.title = element_text(size = 20), axis.title = element_text(size = 20),
-        axis.text = element_text(size = 20)) +
-  labs(x = "", y = "Number of observations", col = "Life stage")
-ggsave("figs/phenocurves_iNat_mothsAndCaterpillars.pdf", units = "in", height = 20, width = 30)
-
-pdf("figs/phenocurves_iNat_mothsAndCaterpillars_pages.pdf", height = 5, width = 8)
-for(i in 1:nrow(bins)) {
-  df <- inat_combined_ids %>%
-    filter(group == i) %>%
-    group_by(lat_bin, lon_bin, life_stage) %>%
-    mutate(n = sum(nObs, na.rm = T))
-  nmoths <- unique(df$n)[[1]]
-  ncats <- unique(df$n)[[2]]
-  location <- paste0(unique(df$lat_bin), ", ", unique(df$lon_bin))
-  plot <- ggplot(df, aes(x = jd_wk, y = nObs, col = life_stage)) +
+for(yr in c(2015:2018)) {
+  ggplot(filter(inat_combined_gather, year == yr), aes(x = jd_wk, y = nObs, col = life_stage)) +
     geom_line(cex = 1) + 
     scale_color_manual(values=c("deepskyblue3", "springgreen3"), 
                        labels = c("caterpillars" = "Caterpillars", "moths" = "Moths")) +
     scale_y_log10() +
+    facet_grid(forcats::fct_reorder(factor(lat_bin), desc(lat_bin))~lon_bin) +
     scale_x_continuous(breaks = jds, labels = dates) +
-    labs(x = "", y = "Number of observations", col = "Life stage") +
-    theme(legend.text = element_text(size = 15), 
-          legend.title = element_text(size = 15), 
-          axis.title = element_text(size = 15),
-          axis.text = element_text(size = 15)) +
-    ggtitle(location)
-  plot2 <- plot_grid(plot, labels = c(paste0("Caterpillars = ", as.character(ncats))),
-                     label_x = c(0.69), label_y = c(0.3))
-  plot3 <- plot_grid(plot2, labels = c(paste0("Moths = ", as.character(nmoths))),
-                     label_x = c(0.72), label_y = c(0.25))
-  print(plot3) # fix dimensions
+    theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 12),
+          strip.text = element_text(size = 20), legend.text = element_text(size = 20),
+          legend.title = element_text(size = 20), axis.title = element_text(size = 20),
+          axis.text = element_text(size = 20)) +
+    labs(x = "", y = "Number of observations", col = "Life stage")
+  ggsave(paste0("figs/inaturalist/phenocurves_iNat_mothsAndCaterpillars_", yr, ".pdf"), units = "in", height = 20, width = 30)
+  
+  bins_yr <- bins %>%
+    filter(year == yr)
+  
+  pdf(paste0("figs/inaturalist/phenocurves_iNat_mothsAndCaterpillars_pages_", yr, ".pdf"), height = 5, width = 8)
+  for(i in bins_yr$group) {
+    df <- inat_combined_ids %>%
+      filter(year == yr) %>%
+      filter(group == i) %>%
+      group_by(lat_bin, lon_bin, life_stage) %>%
+      mutate(n = sum(nObs, na.rm = T))
+    nmoths <- unique(df$n)[[1]]
+    ncats <- unique(df$n)[[2]]
+    location <- paste0(unique(df$lat_bin), ", ", unique(df$lon_bin))
+    plot <- ggplot(df, aes(x = jd_wk, y = nObs, col = life_stage)) +
+      geom_line(cex = 1) + 
+      scale_color_manual(values=c("deepskyblue3", "springgreen3"), 
+                         labels = c("caterpillars" = "Caterpillars", "moths" = "Moths")) +
+      scale_y_log10() +
+      scale_x_continuous(breaks = jds, labels = dates) +
+      labs(x = "", y = "Number of observations", col = "Life stage") +
+      theme(legend.text = element_text(size = 15), 
+            legend.title = element_text(size = 15), 
+            axis.title = element_text(size = 15),
+            axis.text = element_text(size = 15)) +
+      ggtitle(location)
+    plot2 <- plot_grid(plot, labels = c(paste0("Caterpillars = ", as.character(ncats))),
+                       label_x = c(0.69), label_y = c(0.3))
+    plot3 <- plot_grid(plot2, labels = c(paste0("Moths = ", as.character(nmoths))),
+                       label_x = c(0.72), label_y = c(0.25))
+    print(plot3) # fix dimensions
+  }
+  dev.off()
+  
 }
-dev.off()
+
+# 10% accumulation
+
+# two models: catdate ~ mothdate*lat + year
+# catdate - mothdate ~ lat + year
+
+# Cross correlations
+# funs: library(timeseries), adf.test() for stationarity, acf() timelag plot, ccf() compare two time series - which lags highest correlation
