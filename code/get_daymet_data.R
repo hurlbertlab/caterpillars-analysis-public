c3e635a29a571aa00abf80d90a7028723f375050
Di Cecco <gdicecco@ad.unc.edu>
2019-04-30 14:08:53 -0400

    script for getting climate data - change paths

--- code/get_daymet_data.R
+++ code/get_daymet_data.R
@@ -7,25 +7,28 @@ library(ncdf4)
 library(lubridate)
 library(sf)
 
+setwd("\\\\BioArk\\HurlbertLab\\GIS\\")
+
 # Daymet: eastern North America, monthly average temperatures for March-May
 
 download_daymet_ncss(location = c(49, -101, 24, -69),
                       start = 2015,
                       end = 2018,
                       param = "tmin", 
-                      path = "C:/Users/gdicecco/Desktop/data/daymet/")
+                      path = "daymet/")
 
-nc2tif(path = "C:/Users/gdicecco/Desktop/data/daymet/")
+nc2tif(path = "daymet/")
 
-daymet_raster <- stack("C:/Users/gdicecco/Desktop/data/daymet/tmin_monavg_2017_ncss.tif")
+# Read in data for pre-2018
+daymet_raster <- stack("daymet/tmin_monavg_2017_ncss.tif")
 
 daymet_spring <- daymet_raster[[3:5]]
 daymet_points <- rasterToPoints(daymet_spring)
 
 # Need to convert 2018 to monthly averages
 
-nc <- nc_open("C:/Users/gdicecco/Desktop/data/daymet/daymet_v3_tmin_2018_na.nc4")
-na18 <- brick("C:/Users/gdicecco/Desktop/data/daymet/daymet_v3_tmin_2018_na.nc4", varname = "tmin")
+nc <- nc_open("daymet/daymet_v3_tmin_2018_na.nc4")
+na18 <- brick("daymet/daymet_v3_tmin_2018_na.nc4")
 
 # Check if any -999 values - change to NAs
 
@@ -48,10 +51,15 @@ spring18 <- stack(march, april, may)
 eNA_mu <- extent(c(-1000000, 3000000, -2000000, 2000000))
 spring18_eNA <- crop(spring18, eNA_mu)
 names(spring18_eNA) <- c("march2018", "april2018", "may2018")
-writeRaster(spring18_eNA, "C:/Users/gdicecco/Desktop/data/daymet/tmin_spring_2018")
+writeRaster(spring18_eNA, "daymet/tmin_spring_2018")
 
-spring18_eNA <- stack("C:/Users/gdicecco/Desktop/data/daymet/tmin_spring_2018")
-spring18_points <- data.frame(rasterToPoints(spring18_eNA))
-spring18_sf <- st_as_sf(spring18_points, coords = c("x", "y"), crs = "+proj=lcc +lon_0=-100 +lat_0=42.5 +x_0=0 +y_0=0 +lat_1=25 +a=60 +rf=6378137 +lat_2=45")
+spring18_eNA <- stack("daymet/tmin_spring_2018")
 
-spring18_transf <- st_transform(spring18_sf, crs = 4326)
+spring18_eNA_coarse <- aggregate(spring18_eNA, fact = 300)
+
+# This all doesn't work
+spring18_transf <- projectRaster(spring18_eNA_coarse, daymet_spring)
+spring18_points <- data.frame(rasterToPoints(spring18_eNA_coarse))
+spring18_sf <- st_as_sf(spring18_points, coords = c("x", "y"), 
+                        crs = "+proj=lcc +lon_0=-100 +lat_0=42.5 +x_0=0 +y_0=0 +lat_1=25 +a=60 +rf=6378137 +lat_2=45") %>%
+  st_transform(crs = 4269)
