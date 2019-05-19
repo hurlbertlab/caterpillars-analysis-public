2dc305aca07b99a6f99f228ae75a82aae2e46290
gdicecco <gdicecco@live.unc.edu>
2019-05-19 11:25:08 -0400

    Combine caterpillar and moth data

---
+++ code/combine_inat_cats_adults.R
@@ -0,0 +1,47 @@
+### Combine iNat moths and caterpillars, get species by grid cell
+
+library(tidyverse)
+library(cowplot)
+library(tmap)
+library(lubridate)
+library(sf)
+library(zoo)
+library(mgcv)
+library(raster)
+
+# Read in data
+
+inat_moths <- read.csv("data/inat_moths.csv", header = T)
+
+inat_cats <- read.csv('data/inat_caterpillars_easternNA.csv', header = TRUE)
+
+cats <- inat_cats %>%
+  left_join(inat_species, by = "scientific_name") %>%
+  filter(family %in% c("Erebidae", "Geometridae", "Noctuidae", "Notodontidae")) %>%
+  mutate(Date = as.Date(observed_on, format = "%Y-%m-%d")) %>%
+  mutate(year = year(Date), 
+         jday = yday(Date)) %>%
+  filter(year >= 2015) %>%
+  mutate(lat_bin = 2*floor(latitude/2) + 2/2,
+         lon_bin = 2*floor(longitude/2) + 2/2,
+         jd_wk = 7*floor(jday/7))
+
+inat_adults <- inat_moths %>%
+  filter(!(id %in% inat_cats$id)) %>%
+  mutate(Date = as.Date(observed_on, format = "%m/%d/%Y")) %>%
+  mutate(year = year(Date), 
+         jday = yday(Date)) %>%
+  filter(year >= 2015) %>%
+  mutate(lat_bin = 2*floor(latitude/2) + 2/2,
+         lon_bin = 2*floor(longitude/2) + 2/2,
+         jd_wk = 7*floor(jday/7))
+
+# Combine adult and caterpillar datasets
+
+all_inat <- cats %>%
+  dplyr::select(id, observed_on, place_guess, latitude, longitude, scientific_name,
+                common_name, taxon_id, family, Date, year, jday, lat_bin, lon_bin, jd_wk) %>%
+  rename(taxon_family_name = "family") %>%
+  bind_rows(dplyr::select(inat_adults, -place_county_name))
+write.csv(all_inat, "inat_adults_cats_ENA.csv", row.names = F)
+
