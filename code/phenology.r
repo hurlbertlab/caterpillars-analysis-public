c35481856a3db1352ba4ed3aaba8e0266e4de56c
Hurlbert <hurlbert@bio.unc.edu>
2019-08-22 22:08:52 -0400

    generalizing phenology plotting functions, 2019 plots

--- code/phenology.r
+++ code/phenology.r
@@ -14,76 +14,88 @@ source('code/reading_datafiles_without_users.r')
 minNumRecords = 40 
 minNumDates = 4
 
-siteList = fullDataset %>%
-  filter(Year == 2018) %>%
-  group_by(Name, Region, Latitude, Longitude) %>%
-  summarize(nRecs = n_distinct(ID),
-            nDates = n_distinct(LocalDate)) %>%
-  arrange(desc(Latitude)) %>%
-  filter(nRecs >= minNumRecords, nDates >= minNumDates, Name != "Example Site") %>%
-  mutate(county = latlong2county(data.frame(lon = Longitude, lat = Latitude)))
-
-write.table(siteList, 'data/revi/sitelist2018.txt', sep = '\t', row.names = F)
-
-
-
-pdf('figs/caterpillarPhenologyAllSites2018.pdf', height = 8.5, width = 11)
-par(mfrow = c(4, 6), mar = c(2, 2, 2, 1), oma = c(5, 5, 0, 0))
-
-for (site in siteList$Name) {
-  sitedata = fullDataset %>%
-    filter(Name == site, Year == 2018)
+siteList = function(dataset, year, write = TRUE) {
+  out = dataset %>%
+    filter(Year == year) %>%
+    group_by(Name, Region, Latitude, Longitude) %>%
+    summarize(nRecs = n_distinct(ID),
+              nDates = n_distinct(LocalDate)) %>%
+    arrange(desc(Latitude)) %>%
+    filter(nRecs >= minNumRecords, nDates >= minNumDates, Name != "Example Site") %>%
+    mutate(county = latlong2county(data.frame(lon = Longitude, lat = Latitude)))
   
-  if (nchar(site) > 26) {
-    siteLabel = paste(substr(site, 1, 21), "\n", substr(site, 22, nchar(site)), 
-                      ", ", siteList$Region[siteList$Name == site], sep = "")
-  } else {
-    siteLabel = paste(site, ", ", siteList$Region[siteList$Name == site], sep = "")
+  if (write) {
+    write.table(out, paste('data/sitelist', year, '.txt', sep = ''), sep = '\t', row.names = F)
   }
-  
-  
-  # x-axis labels
-  jds = c(1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335)
-  dates = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
-  
-  # make sure xlim endpoints coincide with month labels
-  if(length(unique(sitedata$julianday)) == 1) {
-    minPos = which(jds == max(jds[jds <= min(sitedata$julianday)]))
-    maxPos = which(jds == min(jds[jds >= max(sitedata$julianday)]))
-  } else {
-    minPos = max(which(jds == min(jds[jds >= min(sitedata$julianday)])) - 1, 1)    
-    maxPos = min(which(jds == max(jds[jds <= max(sitedata$julianday)])) + 1, 12)
-  }
-  
-  # Caterpillar phenology
-  caterpillarPhenology = meanDensityByDay(sitedata, ordersToInclude = 'caterpillar', 
-                                          plot = FALSE, plotVar = 'fracSurveys')
-  
-  
-  caterpillarPhenology = meanDensityByDay(sitedata, ordersToInclude = 'caterpillar', 
-                                          plot = TRUE, plotVar = 'fracSurveys', xlab = 'Date',
-                                          ylab = 'Percent of surveys', col = 'red', lwd = 3, 
-                                          xaxt = 'n', cex.lab = 1.5, cex.axis = 1.3,
-                                          xlim = c(jds[minPos], jds[maxPos]),
-                                          ylim = c(0, max(1, 1.3*max(caterpillarPhenology$fracSurveys))), 
-                                          main = siteLabel, cex.main = .9)
-  
-  legend("topright", legend = paste(round(siteList$Latitude[siteList$Name == site], 1), "°N", sep = ""), 
-         bty = 'n')
-  legend("topleft", legend = paste(siteList$nRecs[siteList$Name == site], "surveys"), 
-         bty = 'n', text.col = 'blue', cex = .9)
-  axis(1, at = jds[minPos:maxPos], labels = F, tck = -.03)
-  axis(1, at = jds[minPos:maxPos] + 14, labels = F, tck = -.02)
-  
-  monthLabs = minPos:(maxPos-1)
-  rect(jds[monthLabs[monthLabs%%2 == 0]], rep(-10, length(monthLabs[monthLabs%%2 == 0])), 
-       jds[monthLabs[monthLabs%%2 == 0] + 1]-1, rep(110, length(monthLabs[monthLabs%%2 == 0])), 
-       col = rgb(.1, .1, .1, .1), border = NA)
-  mtext(dates[monthLabs], 1, at = jds[monthLabs]+14, cex = .7, line = .25)
+  return(out)
 }
-mtext("Date", 1, outer = TRUE, line = 1, cex = 1.5)
-mtext("Percent of surveys with caterpillars", 2, outer = TRUE, line = 1, cex = 1.5)
-dev.off()
+
+multiSitePhenoPlot = function(fullDataset, year, siteList, write = TRUE) {
+
+  if (write) {
+    pdf(paste('figs/caterpillarPhenologyAllSites', year, '.pdf', sep = ''), height = 8.5, width = 11)
+  }
+  par(mfrow = c(4, 6), mar = c(2, 2, 2, 1), oma = c(5, 5, 0, 0))
+  
+  for (site in siteList$Name) {
+    sitedata = fullDataset %>%
+      filter(Name == site, Year == year)
+    
+    if (nchar(site) > 26) {
+      siteLabel = paste(substr(site, 1, 21), "\n", substr(site, 22, nchar(site)), 
+                        ", ", siteList$Region[siteList$Name == site], sep = "")
+    } else {
+      siteLabel = paste(site, ", ", siteList$Region[siteList$Name == site], sep = "")
+    }
+    
+    
+    # x-axis labels
+    jds = c(1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335)
+    dates = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+    
+    # make sure xlim endpoints coincide with month labels
+    if(length(unique(sitedata$julianday)) == 1) {
+      minPos = which(jds == max(jds[jds <= min(sitedata$julianday)]))
+      maxPos = which(jds == min(jds[jds >= max(sitedata$julianday)]))
+    } else {
+      minPos = max(which(jds == min(jds[jds >= min(sitedata$julianday)])) - 1, 1)    
+      maxPos = min(which(jds == max(jds[jds <= max(sitedata$julianday)])) + 1, 12)
+    }
+    
+    # Caterpillar phenology
+    caterpillarPhenology = meanDensityByDay(sitedata, ordersToInclude = 'caterpillar', 
+                                            plot = FALSE, plotVar = 'fracSurveys')
+    
+    
+    caterpillarPhenology = meanDensityByDay(sitedata, ordersToInclude = 'caterpillar', 
+                                            plot = TRUE, plotVar = 'fracSurveys', xlab = 'Date',
+                                            ylab = 'Percent of surveys', col = 'red', lwd = 3, 
+                                            xaxt = 'n', cex.lab = 1.5, cex.axis = 1.3,
+                                            xlim = c(jds[minPos], jds[maxPos]),
+                                            ylim = c(0, max(1, 1.3*max(caterpillarPhenology$fracSurveys))), 
+                                            main = siteLabel, cex.main = .9)
+    
+    legend("topright", legend = paste(round(siteList$Latitude[siteList$Name == site], 1), "°N", sep = ""), 
+           bty = 'n')
+    legend("topleft", legend = paste(siteList$nRecs[siteList$Name == site], "surveys"), 
+           bty = 'n', text.col = 'blue', cex = .9)
+    axis(1, at = jds[minPos:maxPos], labels = F, tck = -.03)
+    axis(1, at = jds[minPos:maxPos] + 14, labels = F, tck = -.02)
+    
+    monthLabs = minPos:(maxPos-1)
+    rect(jds[monthLabs[monthLabs%%2 == 0]], rep(-10, length(monthLabs[monthLabs%%2 == 0])), 
+         jds[monthLabs[monthLabs%%2 == 0] + 1]-1, rep(110, length(monthLabs[monthLabs%%2 == 0])), 
+         col = rgb(.1, .1, .1, .1), border = NA)
+    mtext(dates[monthLabs], 1, at = jds[monthLabs]+14, cex = .7, line = .25)
+  }
+  mtext("Date", 1, outer = TRUE, line = 1, cex = 1.5)
+  mtext("Percent of surveys with caterpillars", 2, outer = TRUE, line = 1, cex = 1.5)
+
+  if (write) {
+    dev.off()
+  }
+}  
+  
 
 
 # Top 12 sites (but without ECU)
