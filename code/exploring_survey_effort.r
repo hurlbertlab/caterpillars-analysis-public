61d14dfb0f6f7169f12c06fd98cca1c2def41499
Allen Hurlbert <hurlbert@bio.unc.edu>
2019-10-04 16:25:19 -0400

    adding site labels to site effort plot

--- code/exploring_survey_effort.r
+++ code/exploring_survey_effort.r
@@ -5,6 +5,7 @@ library(lubridate)
 library(sp)
 library(maps)
 library(maptools)
+library(tidyr)
 
 source('code/analysis_functions.r')
 source('code/reading_datafiles_without_users.r')
@@ -33,7 +34,9 @@ highEffortSites = filter(ss,
 
 # Plotting number of surveys per week at each site
 sspw = siteSurveysPerWeek(fullDataset, 2019)
-plot(c(80, 270), c(1, nrow(sspw)), type = 'n', xlab = 'Julian day', ylab = 'Site')
+par(mar = c(4, 15, 1, 1))
+plot(c(80, 270), c(1, nrow(sspw)), type = 'n', xlab = 'Julian day', ylab = '', yaxt = 'n')
 sapply(1:nrow(sspw), function(x) points(as.numeric(names(sspw)[2:ncol(sspw)]), rep(x, ncol(sspw)-1), 
                                 cex = unlist(log(sspw[x, 2:ncol(sspw)]))/2, pch = 16, 
                                 col = rainbow(nrow(sspw))[x]))
+mtext(substr(sspw$Name, 1, 40), 2, at = 1:nrow(sspw), las = 1, line = 1, cex = .8)
