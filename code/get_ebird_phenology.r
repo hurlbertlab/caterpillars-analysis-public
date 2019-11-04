38c5e9e1ed642ba2a313ccc002b76e1abf830ca8
Allen Hurlbert <hurlbert@bio.unc.edu>
2019-11-04 09:00:15 -0500

    saving REVI phenology data

--- code/get_ebird_phenology.r
+++ code/get_ebird_phenology.r
@@ -14,8 +14,8 @@ revi_pheno_summary = data.frame(speciesCode = NA, county = NA, Year = NA, mateda
 
 for (r in 1:nrow(countyYears)) {
   
-  yr = countyYears$ebirdCounty[r]
-  cty = countyYears$Year[r]
+  yr = countyYears$Year[r]
+  cty = countyYears$ebirdCounty[r]
   tmp = getEbirdBarchartData(countyCode = cty, year = yr)
   latitude = sites2$Latitude[sites2$ebirdCounty == cty][1]
 
@@ -23,13 +23,14 @@ for (r in 1:nrow(countyYears)) {
   m2 = matedateCalc2(tmp, dipFromPeak = 0.1)
   
   revi_phenology = rbind(revi_phenology, tmp)
-  revi_phenology_summary = rbind(revi_phenology_summary, data.frame(speciesCode = tmp$speciesCode[1], 
+  revi_pheno_summary = rbind(revi_pheno_summary, data.frame(speciesCode = tmp$speciesCode[1], 
                                                             county = cty, 
                                                             Year = yr, 
                                                             matedate1 = m1, 
                                                             matedate2 = m2))
 }
 revi_phenology = revi_phenology[-1, ]
-revi_phenology_summary = revi_phenology_summary[-1, ]
+revi_pheno_summary = revi_pheno_summary[-1, ]
 
-write.table(revi_phenology, 'data/revi/revi_phenology_by_county.txt', sep = '\t', row.names = F)
+write.table(revi_phenology, 'data/revi/revi_phenology_by_county_thru2019.txt', sep = '\t', row.names = F)
+write.table(revi_phenology, 'data/revi/revi_pheno_summary_by_county_thru2019.txt', sep = '\t', row.names = F)
