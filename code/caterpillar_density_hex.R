94d57e20a668a124e5fc1f735a3325a07ac81b8e
gdicecco <gdicecco@live.unc.edu>
2019-11-09 12:13:39 -0500

    Aggregate caterpillars count data density at the hex cell level

---
+++ code/caterpillar_density_hex.R
@@ -0,0 +1,28 @@
+## Caterpillars survey density in hex cells, 1 degree grid cells
+
+library(tidyverse)
+library(dggridR)
+library(sf)
+
+# Get CC dataset
+source("code/analysis_functions.R")
+
+# Read in hex data
+hex <- st_read("data/maps/hexgrid_materials/hex_grid_crop.shp")
+
+# Number of weeks per site per year per hex cell
+cc_sf <- fullDataset %>% 
+  st_as_sf(coords = c("Longitude", "Latitude")) %>%
+  st_set_crs("+proj=longlat +datum=WGS84 +no_defs") %>%
+  st_intersection(hex)
+
+cc_hex_density <- cc_sf %>%
+  group_by(cell, Year, Name) %>%
+  summarize(sampling_weeks = n_distinct(julianweek))
+
+cc_hex_density_df <- cc_hex_density %>%
+  st_set_geometry(NULL)
+# write.csv(cc_hex_density_df, "data/CatCount_density_hex_cells.csv", row.names = F)
+  
+# Number of weeks per site per year per 1x1 degree grid cell
+
