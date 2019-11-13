13d3a6a03d92ecf14f78b751b990df1533f84818
Allen Hurlbert <hurlbert@bio.unc.edu>
2019-11-13 17:16:55 -0500

    env data update

--- code/get_climate_veg_npn_data.r
+++ code/get_climate_veg_npn_data.r
@@ -9,6 +9,19 @@ source('code/reading_datafiles_without_users.r')
 
 
 ################################################################
+# Daymet
+
+daymet = read.csv('data/env/daymet_climate.csv', header = T)
+
+daymetGDD = climatedata %>%
+  group_by(Name, year) %>%
+  summarize(daymetGDD_May1 = gddCalc(tmean, base = 0, asOfJD = 121),
+            daymetGDD_Jun1 = gddCalc(tmean, base = 0, asOfJD = 152),
+            date2000GDD = dateOfGDDaccumulation(tmean, base = 0, accumulateTo = 2000),
+            precipThruJun1 = sum(prcp..mm.day.[yday <= 152], na.rm = T))
+
+
+################################################################
 # GDD and NPN spring index
 #
 siteyears = fullDataset %>% 
@@ -38,6 +51,7 @@ for (i in 1:length(gddyears)) {
 }
 gdd = bind_rows(gddout)
 
-envData = left_join(leaf, gdd[, c('Name', 'Year', 'npnGDD_May1')], by = c('Name', 'Year'))
+envData = left_join(leaf, gdd[, c('Name', 'Year', 'npnGDD_May1')], by = c('Name', 'Year')) %>% 
+  left_join(daymetGDD, by = c('Name', 'Year' = 'year'))
 
 write.csv(envData, 'data/env/npn_gdd_firstleaf.csv', row.names = F)
