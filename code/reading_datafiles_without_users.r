0f5baf76ad18251df6ba197b5d3dfd77ad9f2bd5
Hurlbert <hurlbert@bio.unc.edu>
2020-07-30 15:26:43 -0400

    removing db files (now in other repo)

--- code/reading_datafiles_without_users.r
+++ code/reading_datafiles_without_users.r
@@ -31,9 +31,7 @@ data_repo <- "https://github.com/hurlbertlab/caterpillars-count-data"
 webpage <- read_html(data_repo)
 repo_links <- html_attr(html_nodes(webpage, "a"), "href")
 data_links <- tibble(link = repo_links[grepl(".csv", repo_links)]) %>%
-  mutate(file_name = word(link, 6, 6, sep = "/"),
-         day = yday(as.Date(substr(link, 1, 10), format = "%Y-%m-%d"))) %>%
-  filter(day == max(day))
+  mutate(file_name = word(link, 6, 6, sep = "/"))
 
 
 ## Read data files from data repo links
