d6cf9f03105b8a4aba0c8b6b3b11b9c6262638e0
Michael Belitz <michaelbelitz06@gmail.com>
2020-11-30 16:43:54 -0500

    updated Figure for MS draft 1

--- iNatUserBehavior/code/inat_ms_figures_mwb.R
+++ iNatUserBehavior/code/inat_ms_figures_mwb.R
@@ -17,39 +17,72 @@ library(gridExtra)
 library(ggrepel)
 
 ## ggplot theme
-theme_set(theme_classic())
+theme_set(theme_classic(base_size = 18))
 
 #### Building Necessary Sub-Panels for Figure 1 ###
 
 ## Annual growth Rate of iNat Use
 
-annual_growth <- read.csv("data/inat_thru_2019_annual_growth.csv", stringsAsFactors = F)
+annual_growth <- read.csv("iNatUserBehavior/data/inat_thru_2019_annual_growth.csv", stringsAsFactors = F)
 
-obs_year <- ggplot() +
-  geom_line(annual_growth, mapping = aes(x = year, y = n_obs), size = 1.25) +
-  labs(x = "Year", y = "Observations") + 
-  scale_x_continuous(breaks = c(2008:2019)) +
-  scale_y_continuous(breaks = c(0, 2500000, 5000000, 7500000,
-                                10000000, 12500000)) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+annual_growth_plot <- ggplot() +
+  geom_line(annual_growth, mapping = aes(x = year, y = n_users/ 100000), 
+            size = 1.25, color = "Purple") +
+  geom_line(annual_growth, mapping = aes(x = year, y = n_obs / 1000000), 
+            size = 1.25, color = "Black") +
+  labs(x = "Year", y = " Unique Users") + 
+  scale_x_continuous(breaks = c(2008, 2010, 2012, 2014, 2016, 2018))  + 
+  scale_y_continuous(name = "Observations (in Millions)", 
+                     breaks = c(0, 2, 4, 6, 8, 10, 12), 
+                     sec.axis = sec_axis(trans = ~ . / 10 , 
+                                         name = "Users (in Millions)", 
+                                         breaks = c(0,0.2,0.4,0.6, 0.8, 1, 1.2))) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  theme(axis.title.y.right = element_text(color = "Purple"),
+        axis.line.y.right = element_line(color = "Purple"),
+        axis.ticks.y.right = element_line(color = "Purple"),
+        axis.text.y.right = element_text(color = "Purple"))+ 
   ggtitle("A")
 
-users_year <- ggplot() +
-  geom_line(annual_growth, mapping = aes(x = year, y = n_users), size = 1.25) +
-  labs(x = "Year", y = " Unique Users") + 
-  scale_x_continuous(breaks = c(2008:2019))  + 
-  scale_y_continuous(breaks = c(0,100000,200000,300000,400000,500000)) +
-  theme(axis.text.x = element_text(angle = 45, hjust=1)) +
-  ggtitle("C")
 
-growth_cp <- plot_grid(obs_year, users_year, ncol = 1)
-growth_cp
+## 2018 observation phenology
 
+# City nature challenge - jday 119 in 2018
+jds = c(1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335)
+dates = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+
+inat_2018_pheno <- read.csv("iNatUserBehavior/data/inat_2018_annual_user_pheno.csv", stringsAsFactors = F) %>% 
+  mutate(week = 0:52)
+
+pheno <- ggplot() + 
+  geom_line(inat_2018_pheno, mapping = aes(x = week, y = n_obs / 1000), 
+            stat = "identity", size = 1.25) + 
+  geom_line(inat_2018_pheno, mapping = aes(x = week, y = n_users / 300), 
+            stat = "identity", size = 1.25, color = "purple") + 
+  labs(x = "Week of the Year", y = "Observations") +
+  scale_y_continuous(name = "Observations (in Thousands)", 
+                     breaks = c(0, 200, 400, 600),
+                     sec.axis = sec_axis(trans = ~ . /3.3333,
+                                         name = "Users (in Thousands)")) +
+  theme(axis.title.y.right = element_text(color = "Purple"),
+        axis.line.y.right = element_line(color = "Purple"),
+        axis.ticks.y.right = element_line(color = "Purple"),
+        axis.text.y.right = element_text(color = "Purple")) +
+  ggtitle("B")
+
+growth_cp <- cowplot::plot_grid(annual_growth_plot, pheno, ncol = 2)
 
 #### spatial Map of observations ####
 
 ## Map of observations by country
 
+obs1 <- readRDS("iNatUserBehavior/bigdata/inat_records_by_countries1.rds")
+obs2 <- readRDS("iNatUserBehavior/bigdata/inat_records_by_countries2.rds")
+obs3 <- readRDS("iNatUserBehavior/bigdata/inat_records_by_countries3.rds")
+
+inat_world <- rbind(obs1, obs2, obs3) %>% 
+  st_drop_geometry()
+
 data(wrld_simpl)
 
 world <-  wrld_simpl %>% 
@@ -59,8 +92,6 @@ data(wrld_simpl)
 
 world <-  wrld_simpl %>% 
   st_as_sf()
-
-inat_world <- readRDS("bigdata/inat_records_perCountry.rds")
 
 world2 <- left_join(world, inat_world) %>% 
   mutate(percapita = count/POP2005) %>% 
@@ -69,10 +100,11 @@ world2 <- left_join(world, inat_world) %>%
 global_obs <- ggplot() + 
   geom_sf(world2, mapping = aes(fill = count)) +
   scale_fill_viridis_c(trans = "log10", breaks = c(1,1000,1000000),
-                       guide = guide_colorbar(title.position = "top")) +
+                       guide = guide_colorbar(title.position = "top"),
+                       labels = c(1, 1000, "1 Mill")) +
   labs(fill = "Observations") +
   theme(legend.position = "bottom") +
-  ggtitle("E")
+  ggtitle("C")
 
 
 ## Land Cover Bias Plot
@@ -84,12 +116,12 @@ nlcd_palette <- c("Open Water" = "#788cbe", "Developed Open Space" = "#dec9c9", 
                   "Barren Land (Rocky/Sand/Clay)" = "#b3ada3",
                   "Deciduous Forest" = "#69ab63", "Evergreen Forest" = "#1c6330", "Mixed Forest" = "#b5c98f",
                   "Shrub/Scrub" = "#ccba7d", "Grassland/Herbaceous" = "#e3e3c2", "Pasture/Hay" = "#dbd93d",
-                  "Cultivated Crops" = "#ab7029", "Woody Wetlands" = "#bad9eb", "Emergent Herbaceous Wetlands" = "#70a3ba")
+                  "Cultivated Crops" = "#ab7029", "Woody Wetlands" = "#bad9eb", "Herbaceous Wetlands" = "#70a3ba")
 
 nlcd_legend <- read.csv("data/nlcd2016_legend.csv", stringsAsFactors = F)
 
 # Land cover for iNat observation sites, 0's and NA are no data
-inat_landcover <- read.csv("data/inat_site_landcover.csv", stringsAsFactors = F)
+inat_landcover <- read.csv("iNatUserBehavior/bigdata/inat_us_site_landcover.csv", stringsAsFactors = F)
 
 inat_lc_group <- inat_landcover %>% 
   group_by(landcover) %>% 
@@ -97,13 +129,16 @@ inat_lc_group <- inat_landcover %>%
   filter(!is.na(landcover)) %>% 
   filter(landcover != 0)
 
-mlrc_prop <- read.csv("data/env/mrlc_proportions.csv")
+mlrc_prop <- read.csv("iNatUserBehavior/data/mrlc_proportions.csv")
 
 inat_lc_group2 <- left_join(inat_lc_group, mlrc_prop, by = c("landcover" = "X")) %>% 
   dplyr::rename(true_perc = Percentage) %>% 
   mutate(obs_perc = count / sum(inat_lc_group$count)*100) %>% 
   rename("Land_Cover_Class" = 3) %>% 
-  mutate(diff = true_perc - obs_perc)
+  mutate(diff = true_perc - obs_perc) %>% 
+  filter(landcover != 12)
+
+inat_lc_group2[15,3] <- "Herbaceous Wetlands"
 
 inat_lc_group2$Land_Cover_Class <- as.factor(inat_lc_group2$Land_Cover_Class)
 
@@ -119,7 +154,7 @@ inat_lc_group2$Land_Cover_Class <- factor(inat_lc_group2$Land_Cover_Class,
                                             "Pasture/Hay",
                                             "Cultivated Crops",
                                             "Woody Wetlands",
-                                            "Emergent Herbaceous Wetlands",
+                                            "Herbaceous Wetlands",
                                             "Bare Rock/Sand/Clay",
                                             "Water",
                                             "Grasslands/Herbaceous",
@@ -133,59 +168,41 @@ lc_plot <- ggplot(inat_lc_group2) +
            aes (y = Land_Cover_Class, x = true_perc, color = "Expected Percentage"), fill = "transparent") +
   scale_color_manual(values = "Black")  +
   scale_fill_manual(values = c("darkorange3", "grey", "orangered3", "chartreuse4",
-                               "yellow", "tan", "dodger blue", "light blue"),
+                               "yellow", "tan", "dodger blue", "light blue", "Pink"),
                     name = "Generalized Class") +
   scale_x_continuous(expand = c(0,0)) + 
   labs(x = "Percentage of Observations", y = "") + 
   labs(color = "") +
-  ggtitle("F")
+  ggtitle("D")
 
 # Land cover distribution for whole US
 #https://www.mrlc.gov/data/statistics/national-land-cover-database-2016-nlcd2016-statistics-2016
 
-
-## 2018 observation phenology
-
-# City nature challenge - jday 119 in 2018
-jds = c(1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335)
-dates = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
-
-inat_2018_pheno <- read.csv("data/inat_2018_annual_user_pheno.csv", stringsAsFactors = F)
-
-obs_pheno <- ggplot() + 
-  geom_bar(inat_2018_pheno, mapping = aes(x = jd_wk, y = n_obs), stat = "identity") + 
-  labs(x = "Day of year", y = "Observations") +
-  scale_y_continuous(expand = c(0,0)) +
-  ggtitle("B")
-
-users_pheno <- ggplot() + 
-  geom_bar(inat_2018_pheno, mapping = aes(x = jd_wk, y = n_users), stat = "identity") + 
-  labs(x = "Day of year", y = "Unique users") +
-  scale_y_continuous(expand = c(0,0))  +
-  ggtitle("D") 
-
-cnc <- cowplot::plot_grid(obs_pheno, users_pheno, ncol = 1)
-
 ## Grid Arrange these all together for FIg 1 ##
 
-lay <- rbind(c(1,2,4,4),
-             c(1,2,4,4),
-             c(3,3,4,4),
-             c(3,3,4,4))
+lay <- rbind(c(1,1,3,3),
+             c(1,1,3,3),
+             c(2,2,3,3),
+             c(2,2,3,3))
 
-ga <- grid.arrange(growth_cp, cnc, global_obs, lc_plot, layout_matrix = lay, 
-                   widths = c(1,1,1,1.25), heights = c(1,1,1,0.75))
+ga <- grid.arrange(growth_cp, global_obs, lc_plot, 
+                   layout_matrix = lay, heights = c(0.5,0.5, 0.75,1),
+                   widths = c(1,1, 1.25,1))
 
-ggsave(filename = "figs/Figure1.pdf", plot = ga, width = 16, height = 8)
+ggsave(filename = "iNatUserBehavior/figs/Fig1.pdf", plot = ga,
+       width = 16, height = 10, dpi = 450)
+
+ggsave(filename = "iNatUserBehavior/figs/Fig1.png", plot = ga,
+       width = 16, height = 10, dpi = 300)
 
 
 ### Figure 2 - Number of Species Observed Per Class ###
 
 ## 2c) species per class
 
-inat_2019_species <- read.csv("data/inat_2019_spp.csv", stringsAsFactors = F)
+inat_2019_species <- read.csv("iNatUserBehavior/data/inat_taxon_info_thru2019.csv", stringsAsFactors = F)
 
-eol_spp_per_class <- read.csv("data/species_cnts_by_taxon.csv", stringsAsFactors = F)
+eol_spp_per_class <- read.csv("iNatUserBehavior/data/species_cnts_by_taxon.csv", stringsAsFactors = F)
 
 eol2 <- eol_spp_per_class %>% 
   filter(!is.na(class_cnt)) %>% 
@@ -207,7 +224,8 @@ tdf2 <- tdf %>%
                         yes = 0,
                         no = count)) %>% 
   filter(kingdom == "Animalia" | kingdom == "Fungi" | kingdom == "Plantae") %>% 
-  mutate(propObs = count/class_cnt)
+  mutate(propObs = count/class_cnt) %>% 
+  filter(class != "Not assigned")
 
 
 obs_by_class <- ggplot(tdf2) + 
@@ -218,94 +236,50 @@ obs_by_class <- ggplot(tdf2) +
                   label = "Aves") +
   geom_text_repel(filter(tdf2, class == "Ginkgoopsida"), 
                   mapping = aes(class_cnt, propObs), 
-                  label = "Ginkgoopsida", nudge_y = -0.02, nudge_x = -0.03) +
+                  label = "Ginkgoopsida", nudge_y = -0.03) +
   geom_text_repel(filter(tdf2, class == "Insecta"), 
                   mapping = aes(class_cnt, propObs), 
                   label = "Insecta") +
   geom_text_repel(filter(tdf2, class == "Merostomata"), 
                   mapping = aes(class_cnt, propObs), 
-                  label = "Merostomata", nudge_y = 0.1, nudge_x = 0.3) +
+                  label = "Merostomata", nudge_y = 0.03) +
   geom_text_repel(filter(tdf2, class == "Pinopsida"), 
                   mapping = aes(class_cnt, propObs), 
                   label = "Pinopsida") +
-  geom_text_repel(filter(tdf2, class == "Coniocybomycetes"), 
-                  mapping = aes(class_cnt, propObs), 
-                  label = "Coniocybomycetes", nudge_y = 0.05) +
-  geom_text_repel(filter(tdf2, class == "Neolectales"), 
-                  mapping = aes(class_cnt, propObs), 
-                  label = "Neolectales") +
-  geom_text_repel(filter(tdf2, class == "Cephalaspidomorphi"), 
-                  mapping = aes(class_cnt, propObs), 
-                  label = "Cephalaspidomorphi", nudge_y = 0.05) +
   geom_text_repel(filter(tdf2, class == "Reptilia"), 
                   mapping = aes(class_cnt, propObs), 
-                  label = "Reptilia", nudge_x = 0.1) +
+                  label = "Reptilia", nudge_x = 0.03) +
   geom_text_repel(filter(tdf2, class == "Mammalia"), 
                   mapping = aes(class_cnt, propObs), 
-                  label = "Mammalia", nudge_y = 0.1) +
-  geom_text_repel(filter(tdf2, class == "Gnetopsida"), 
+                  label = "Mammalia", nudge_x = 0.03) +
+  geom_text_repel(filter(tdf2, class == "Sarcopterygii"), 
                   mapping = aes(class_cnt, propObs), 
-                  label = "Gnetopsida", nudge_x = -0.2) +
-  geom_text_repel(filter(tdf2, class == "Scyphozoa"), 
+                  label = "Sarcopterygii", nudge_y = -0.03) +
+  geom_text_repel(filter(tdf2, class == "Amphibia"), 
                   mapping = aes(class_cnt, propObs), 
-                  label = "Scyphozoa", nudge_x = -0.4, nudge_y = -0.05) +
-  geom_text_repel(filter(tdf2, class == "Neolectomycetes"), 
+                  label = "Amphibia", nudge_y = -0.03) +
+  geom_text_repel(filter(tdf2, class == "Cycadopsida"), 
                   mapping = aes(class_cnt, propObs), 
-                  label = "Neolectomycetes", nudge_x = -0.2, nudge_y = -0.05) +
-  scale_x_log10() + 
+                  label = "Cycadopsida", nudge_x = -0.05, nudge_y = -0.05) +
+  geom_text_repel(filter(tdf2, class == "Magnoliopsida"), 
+                  mapping = aes(class_cnt, propObs), 
+                  label = "Magnoliopsida", nudge_y = 0.03) +
+  geom_text_repel(filter(tdf2, class == "Liliopsida"), 
+                  mapping = aes(class_cnt, propObs), 
+                  label = "Liliopsida", nudge_y = -0.03) +
+  geom_text_repel(filter(tdf2, class == "Gastropoda"), 
+                  mapping = aes(class_cnt, propObs), 
+                  label = "Gastropoda", nudge_x = 0.03) +
+  geom_text_repel(filter(tdf2, class == "Coniocybomycetes"), 
+                mapping = aes(class_cnt, propObs), 
+                label = "Coniocybomycetes", nudge_y = -0.03) +
+  scale_x_log10(breaks = c(1,10,100,1000,10000,100000),
+                labels=scales::comma_format(accuracy = 1)) + 
   labs(x = "Log of Named Species per Class", 
        y = "Proportion of Species Observed per Class", 
        color = "Phylum") + 
   scale_color_manual(values = c("#33638DFF", "#f68f46ff", "#73D055FF"))
 
-ggsave(filename = "figs/inat_spp_by_class_eol.pdf", plot = obs_by_class, width = 8, height = 6)
+ggsave(filename = "iNatUserBehavior/figs/Fig2.pdf", plot = obs_by_class, width = 8, height = 6)
+ggsave(filename = "iNatUserBehavior/figs/Fig2.png", plot = obs_by_class, width = 8, height = 6)
 
-
-
-#### Figures 3-5: User behavior ####
-
-user_even_order <- read.csv("data/inat_user_evenness_orders.csv", stringsAsFactors = F)
-user_even_class <- read.csv("data/inat_user_evenness_class.csv", stringsAsFactors = F)
-
-inat_user_spp <- read.csv("data/inat_2019_user_spp.csv", stringsAsFactors = F)
-inat_user_obs <- read.csv("data/inat_2019_user_obs.csv", stringsAsFactors = F)
-
-user_specializ <- inat_user_obs %>%
-  left_join(inat_user_spp) %>%
-  left_join(user_even_class) %>%
-  left_join(user_even_order, by = c("user.login"), suffix = c("_class", "_order")) %>%
-  mutate_at(c("shannonE_order", "shannonE_class"), ~ifelse(. == 0, NA, .)) %>%
-  mutate(spp_per_obs = n_spp/total_obs,
-         spp_per_obs_insect = n_spp_insect/total_obs_insect) %>%
-  filter(spp_per_obs <= 1, spp_per_obs_insect <= 1) %>%
-  mutate_at(c("spp_per_obs", "spp_per_obs_insect"), ~ifelse(. == 1, . - 0.00001, .))
-
-class_mod <- lm(log(spp_per_obs/(1 - spp_per_obs)) ~ shannonE_class, data = filter(user_specializ, total_obs > 20))
-order_mod <- lm(log(spp_per_obs_insect/(1 - spp_per_obs_insect)) ~ shannonE_class, data = filter(user_specializ, total_obs_insect > 20))
-
-class_plot <- ggplot(filter(user_specializ, total_obs > 20), aes(x = shannonE_class, y = spp_per_obs)) + 
-  geom_point(alpha = 0.1) + geom_smooth(method = "lm", se= F) +
-  labs(x = "Shannon Evenness", y = "Species per observation") + 
-  theme(plot.margin=unit(c(0.25,0.25,0.25,0.25), "in"))
-
-order_plot <- ggplot(filter(user_specializ, total_obs_insect > 20), aes(x = shannonE_order, y = spp_per_obs_insect)) + 
-  geom_point(alpha = 0.1) + geom_smooth(method = "lm", se= F) +
-  labs(x = "Shannon Evenness", y = "Species per observation") + 
-  theme(plot.margin=unit(c(0.25,0.25,0.25,0.25), "in"))
-
-plot_grid(class_plot, order_plot, nrow = 1, labels = c("A) All Classes", "B) Insect Orders"))
-ggsave("figs/inaturalist/shannon_evenness_scatter.pdf", height = 5, width = 10, units = "in")
-
-shannonE <- ggplot(filter(user_specializ, total_obs > 20), aes(x = shannonE_class)) + 
-  geom_histogram(aes(fill = "All Classes"), alpha = 0.5) + 
-  geom_histogram(data = filter(user_specializ, total_obs_insect > 20), aes(x = shannonE_order, fill = "Insect Orders"), alpha = 0.5) +
-  scale_fill_manual(values = c("All Classes" = "skyblue3", "Insect Orders" = "springgreen3")) +
-  labs(x = "Shannon Evenness", y = "Count", fill = "") + 
-  theme(legend.position = c(0.8, 0.9))
-
-user_insect_all <- ggplot(filter(user_specializ, total_obs > 20, total_obs_insect > 20), aes(x = shannonE_class, y = shannonE_order)) +
-  geom_point(alpha = 0.1) + geom_abline(intercept = 0, slope = 1, col = "blue", lty = 2, cex = 1) +
-  labs(x = "Shannon Evenness - All Classes", y = "Shannon Evenness - Insect Orders")
-
-plot_grid(shannonE, user_insect_all, nrow = 1)
-ggsave("figs/inaturalist/shannon_evenness_distributions.pdf", height = 5, width = 10, units = "in")
