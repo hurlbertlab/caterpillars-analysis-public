6779aab0eaeeab581ace3ff9edc947de83eeabd9
Hurlbert <hurlbert@bio.unc.edu>
2021-07-01 12:32:19 -0400

    removing NC university related code

--- code/iNat_photo_agreement.r
+++ code/iNat_photo_agreement.r
@@ -13,44 +13,29 @@ expert = read.csv(paste(github_raw, filter(data_links, grepl("ExpertIdentificati
 
 
 
-ECU = filter(fullDataset, SiteFK == 67)
-
-NCSU = filter(fullDataset, SiteFK == 68)
-
-# All UNC class observations occurred before the end of April
-UNC = filter(fullDataset, SiteFK == 60, julianday < 120)
-
-NCschools = rbind(ECU, NCSU, UNC)
-
-NCphotos = filter(NCschools, Photo == 1)
-
-expPhotos = filter(expert, ArthropodSightingFK %in% NCphotos$arthID) %>%
-  filter(!OriginalGroup %in% c('other', 'unidentified')) %>%
-  mutate(agree = ifelse(OriginalGroup == StandardGroup, 1, 0)) %>%
-  left_join(NCschools[, c('SiteFK', 'arthID', 'Length')], by = c('ArthropodSightingFK' = 'arthID'))
-
-confusion = filter(expPhotos, agree == 0) %>%
-  mutate(incorrect = paste(OriginalGroup, StandardGroup)) %>%
-  group_by(SiteFK) %>%
-  count(incorrect) %>% 
-  arrange(SiteFK, desc(n))
-
-summary = expPhotos %>%
-  group_by(SiteFK) %>%
-  summarize(agreements = sum(agree),
-            total = n(),
-            pctCorrect = round(100*agreements/total, 1))
-
-summaryLarge = expPhotos %>%
-  filter(Length >= 5) %>%
-  group_by(SiteFK) %>%
-  summarize(agreements = sum(agree),
-            total = n(),
-            pctCorrect = round(100*agreements/total, 1))
-
 
 overallAgreement = expert %>%
   filter(!OriginalGroup %in% c('other', 'unidentified')) %>%
   mutate(agree = ifelse(OriginalGroup == StandardGroup, 1, 0))
 
 100*sum(overallAgreement$agree)/nrow(overallAgreement)
+
+
+confusion = filter(overallAgreement, agree == 0) %>%
+  mutate(incorrect = paste(OriginalGroup, StandardGroup)) %>%
+  count(incorrect) %>% 
+  arrange(desc(n))
+
+summary = overallAgreement %>%
+  group_by(SiteFK) %>%
+  summarize(agreements = sum(agree),
+            total = n(),
+            pctCorrect = round(100*agreements/total, 1))
+
+summaryLarge = overallAgreement %>%
+  filter(Length == 5) %>%
+  group_by(SiteFK) %>%
+  summarize(agreements = sum(agree),
+            total = n(),
+            pctCorrect = round(100*agreements/total, 1))
+
