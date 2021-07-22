f4fd00d0c2de2c03de83d4c06211bd67671556dc
Colleen Whitener <cw17@ad.unc.edu>
2021-07-22 16:09:40 -0400

    created a file to save the practice images for the bar plots of the error rates
    edited the code for iNat_photo_agreement.r adding code for error rates and bar plots

--- code/iNat_photo_agreement.r
+++ code/iNat_photo_agreement.r
@@ -1,3 +1,5 @@
+library(dplyr)
+
 dataFiles = list.files('data')[grep("fullDataset", list.files('data'))]
 mostRecentFile = dataFiles[length(dataFiles)]
 
@@ -51,4 +53,70 @@ errorByTaxon <- function(taxon) {
   mistakes <- givenTaxon %>%
     filter(StandardGroup != OriginalGroup)
   100* nrow(mistakes) / nrow(givenTaxon)
-  }
+}
+
+#write errorByTaxon results for each taxon to a tibble
+errorForTaxa <- tibble(
+  unique(overallAgreement$OriginalGroup),
+  sapply(unique(overallAgreement$OriginalGroup), errorByTaxon)
+  )
+  names(errorForTaxa) <- (c('Taxon', 'Percent_Misidentified'))
+
+#create bar plot for errorForTaxa
+taxonErrorPlot <- barplot(errorForTaxa$Percent_Misidentified, las = 2, font = 1)
+
+#function for calculating percent error at a given site
+errorBySite <- function(site) {
+  givenName <- overallAgreement %>%
+    filter(Name == site)
+  mistakes <- givenName %>%
+    filter(StandardGroup != OriginalGroup)
+  100* nrow(mistakes) / nrow(givenName)
+}
+#write errorBySite results for each Site to a tibble
+errorForSite <- tibble(
+  unique(overallAgreement$Name),
+  sapply(unique(overallAgreement$Name), errorBySite)
+)
+names(errorForSite) <- (c('Site', 'Percent_Misidentified'))
+
+#create bar plot for errorForSite
+siteErrorPlot <- barplot(errorForSite$Percent_Misidentified, las = 2, font = 1)
+
+#function for calculating percent error by a given user
+errorByUserID <- function(UserID) {
+  givenUserFKOfObserver <- overallAgreement %>%
+    filter(UserFKOfObserver == UserID)
+  mistakes <- givenUserFKOfObserver %>%
+    filter(StandardGroup != OriginalGroup)
+  100* nrow(mistakes) / nrow(givenUserFKOfObserver)
+}
+
+#write errorByUserID results for each UserID to a tibble
+errorForUserID <- tibble(
+  unique(overallAgreement$UserFKOfObserver),
+  sapply(unique(overallAgreement$UserFKOfObserver), errorByUserID)
+)
+names(errorForUserID) <- (c('User_ID', 'Percent_Misidentified'))
+
+#create bar plot for errorForUserID
+siteErrorPlot <- barplot(errorForUserID$Percent_Misidentified, las = 2, font = 1)
+
+#function for calculating percent error by a given length
+errorByArthLength <- function(ArthLength) {
+  givenLength <- overallAgreement %>%
+    filter(Length == ArthLength)
+  mistakes <- givenLength %>%
+    filter(StandardGroup != OriginalGroup)
+  100* nrow(mistakes) / nrow(givenLength)
+}
+
+#write errorByArthLength results for each ArthLength to a tibble
+errorForArthLength <- tibble(
+  unique(overallAgreement$Length),
+  sapply(unique(overallAgreement$Length), errorByArthLength)
+)
+names(errorForArthLength) <- (c('ArthLength', 'Percent_Misidentified'))
+
+#create bar plot for errorForArthLength
+siteErrorPlot <- barplot(errorForArthLength$Percent_Misidentified, las = 2, font = 1)
